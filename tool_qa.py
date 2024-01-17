e7bf14c46ea60b61675392cc74a6a648567fdbbb
TuringTux <github.com@turingtux.me>
2024-01-17 15:40:47 +0100

    Improve description

--- tool_qa.py
+++ tool_qa.py
@@ -3,6 +3,6 @@ from llm import llm
 
 tool_qa = Tool.from_function(
     func=llm.invoke,
-    name="QA",
-    description="Tool to answer a question",
+    name="Question answering using own knowledge",
+    description="Attempt to answer any question using only your own knowledge. Note that you might misremember things, so when in doubt, prefer the answer of another tool. But: Before you don't output anything, always use this at least as last resort.",
 )
