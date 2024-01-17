a7ef845292067bce7c0a60674febc778434916a8
TuringTux <github.com@turingtux.me>
2024-01-17 17:09:43 +0100

    Fix stupid error, update prompt

--- agent.py
+++ agent.py
@@ -10,7 +10,16 @@ premade_tools = load_tools(["wolfram-alpha"])
 our_tools = [tool_qa, tool_chroma]
 tools = premade_tools + our_tools
 
-PREFIX = """You are participating in a pubquiz in a pub in Heidelberg, Baden-Württemberg, Germany. Answer factually. If you do not know the answer after trying multiple tools, guess. Keep the answer short! Answer in single worlds when possible, otherwise in a short sentence. Good performance will be rewarded with a tip."""
+PREFIX = """You are participating in a pubquiz. You are given the following information. Please follow the instructions carefully.
+
+- Answer factually. If you do not know the answer, try using and combining multiple tools.
+- If you cannot get the answer using any tool, guess.
+- Some questions are about fictional characters or settings. In this case, answer from an in-universe perspective (e.g. Q: "When was Harry Potter born?" A: "July 31st, 1980")
+- Keep the answer short! Answer in single words whenever possible. (e.g. Q: "Who is current German chancellor", A: "Olaf Scholz")
+- Unless otherwise specified, if there are several correct answers where one is for the common case and another one is niche, answer with the common answer.
+- You are in a pub in Heidelberg, Baden-Württemberg, Germany.
+
+Good performance will be rewarded with a tip. Thanks :)"""
 agent = initialize_agent(
     tools=tools,
     llm=llm,
@@ -26,7 +35,7 @@ agent = initialize_agent(
 
 def _is_unknown(response):
     response_lower = response.lower()
-    return "unknown" in response_lower or "unbekannt" in response_lower or "i don't know" in response_lower
+    return "unknown" in response_lower or "unbekannt" in response_lower or "i don't know" in response_lower or "N/A" in response_lower
 
 
 def get_response(question):
@@ -36,6 +45,7 @@ def get_response(question):
     while retry_times > 0:
         try:
             response = agent.invoke(question)["output"]
+            # TODO: Validierung: Frage ein LLM: Ist diese Antwort eine zufriedenstellende Antwort auf die Frage? Falls nein, formuliere die Frage um, sodass du dich leichter tust, sie zu beantworten.
             if not _is_unknown(response):
                 return response
         except Exception:
