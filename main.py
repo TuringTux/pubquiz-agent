d76e7df186f20c837ea305003d62da53297072e6
TuringTux <github.com@turingtux.me>
2024-01-17 15:01:11 +0100

    Move agent to own file

--- main.py
+++ main.py
@@ -1,41 +1,5 @@
 #!/usr/bin/env python3
-import os
-from dotenv import load_dotenv
-from langchain.chat_models import AzureChatOpenAI
-from langchain.agents import initialize_agent, AgentType
-from langchain.tools import Tool
-
-load_dotenv()
-
-# LLM
-azure_api_key = os.getenv('AZURE_OPENAI_API_KEY')
-azure_endpoint = os.getenv('AZURE_OPENAI_ENDPOINT')
-
-llm = AzureChatOpenAI(
-    api_key=azure_api_key,
-    api_version="2023-05-15",
-    azure_deployment="gpt-35-turbo-16k",
-    azure_endpoint=azure_endpoint,
-)
-
-qa_tool = Tool.from_function(
-    func=llm.invoke,
-    name="QA",
-    description="Tool to answer a question",
-)
-
-PREFIX = """You are participating in a pubquiz. Answer in a short sentence."""
-agent = initialize_agent(
-    tools=[qa_tool],
-    llm=llm,
-    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=False,
-    return_intermediate_steps=False,
-    handle_parsing_errors=True,
-    agent_kwargs={
-        'prefix': PREFIX
-    }
-)
+from agent import agent
 
 while True:
     try:
